bd7eceeb7b1e17bbe54631d51b84a6cb6225e1de
Alibek120699 <sayakalibek1@gmail.com>
2020-03-14 17:28:58 +0600

    feat(auth): add users and authorization

--- main/users/admin.py
+++ main/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import MyUser, Profile
+
+
+class InlineProfile(admin.StackedInline):
+    model = Profile
+    verbose_name = 'Profile'
+    verbose_name_plural = 'Profiles'
+    can_delete = False
+
+
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+    inlines = (InlineProfile, )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'bio')
---
+++ main/users/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import MyUser
+
+
+class MyUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = MyUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password', 'role')
+
+    def create(self, validated_data):
+        new_user = MyUser.objects.create_superuser(**validated_data)
+        return new_user
