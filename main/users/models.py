06eeafaf98f7a88e29ca430097cfdd164ba948c8
alibek <sayakalibek1@gmail.com>
2020-03-18 22:12:52 +0600

    feat(user): add user manager

--- main/users/models.py
+++ main/users/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from .constants import USER_ROLES_CHOICES, STUDENT
+from .constants import USER_ROLES_CHOICES, STUDENT, TEACHER, OFFICE_REGISTER
+
+
+class MyUserManager(models.Manager):
+    def get_teachers(self):
+        return self.filter(role=TEACHER)
+
+    def get_students(self):
+        return self.filter(role=STUDENT)
+
+    def get_office_registers(self):
+        return self.filter(role=OFFICE_REGISTER)
+
+    def get_users(self):
+        return self.all()
 
 
 class MyUser(AbstractUser):
     role = models.PositiveSmallIntegerField(choices=USER_ROLES_CHOICES, default=STUDENT)
+
+    objects = MyUserManager()
+
+    def __str__(self):
+        return f'{self.username}: {self.role}'
 
 
 class Profile(models.Model):
