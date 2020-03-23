896f96cd45d3885d925f27ae2f783bfaded3b575
Alibek120699 <sayakalibek1@gmail.com>
2020-03-23 10:20:49 +0600

    feat(subject): create views, viewsets for api

--- main/api/views.py
+++ main/api/views.py
@@ -1,3 +1,131 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, mixins, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .permissions import IsAllowedToCreateTeacherSubject,\
+    IsAllowedToCreateSubjectStudent, IsAllowedToCreateDiscipline
+from .models import Subject, TeacherSubject, SubjectStudent
+from .serializers import SubjectSerializer, TeacherSubjectSerializer,\
+    SubjectStudentSerializer
+
+
+class TeacherSubjectListCreateAPIView(mixins.CreateModelMixin,
+                                      mixins.ListModelMixin,
+                                      generics.GenericAPIView):
+    http_method_names = ['get', 'post']
+    queryset = TeacherSubject.objects.all()
+    serializer_class = TeacherSubjectSerializer
+    permission_classes = (IsAuthenticated, IsAllowedToCreateTeacherSubject)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class TeacherSubjectViewSet(mixins.RetrieveModelMixin,
+                            mixins.DestroyModelMixin,
+                            mixins.UpdateModelMixin,
+                            viewsets.GenericViewSet):
+    queryset = TeacherSubject.objects.all()
+    serializer_class = TeacherSubjectSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class SubjectStudentListCreateAPIView(mixins.ListModelMixin,
+                                      mixins.CreateModelMixin,
+                                      generics.GenericAPIView):
+    http_method_names = ['get', 'post']
+    queryset = SubjectStudent.objects.all()
+    serializer_class = SubjectStudentSerializer
+    permission_classes = (IsAuthenticated, IsAllowedToCreateSubjectStudent)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class SubjectStudentViewSet(mixins.RetrieveModelMixin,
+                            mixins.DestroyModelMixin,
+                            mixins.UpdateModelMixin,
+                            viewsets.GenericViewSet):
+    queryset = SubjectStudent.objects.all()
+    serializer_class = SubjectStudentSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+class SubjectCreateListAPIView(mixins.CreateModelMixin,
+                               mixins.ListModelMixin,
+                               generics.GenericAPIView):
+    http_method_names = ['post', 'get']
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    permission_classes = (IsAllowedToCreateDiscipline, )
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_success_headers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class SubjectViewSet(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
